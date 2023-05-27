a86fb28f0b5bf8540f9989dd7e6454c997ab802b
Mudit Mathur <muditkmathur@gmail.com>
2023-05-27 17:20:19 +0530

    updated the structure

--- rules/model/filterRule.go
+++ rules/model/filterRule.go
@@ -7,28 +7,47 @@ import (
 	"sort"
 )
 
-type DataTypes string
+type Scenario struct {
+	Version    string                  `json:"version"`
+	ScenarioId string                  `json:"scenario_id"`
+	Workloads  map[string]WorkloadRule `json:"workloads"`
+	Filter     Filter                  `json:"filter"`
+}
+
+type WorkloadRule struct {
+	Service   string    `json:"service,omitempty"`
+	TraceRole TraceRole `json:"trace_role,omitempty"`
+	Protocol  Protocol  `json:"protocol,omitempty"`
+	Rule      Rule      `json:"rule,omitempty"`
+}
+
+type Rule struct {
+	Type string `json:"type"`
+	*RuleGroup
+	*RuleLeaf
+}
+
+type RuleGroup struct {
+	Condition *Condition `json:"condition,omitempty"`
+	Rules     []Rule     `json:"rules,omitempty"`
+	Type      string     `json:"type"`
+}
+
+type RuleLeaf struct {
+	ID       *string        `json:"id,omitempty"`
+	Field    *string        `json:"field,omitempty"`
+	Datatype *DataType      `json:"datatype,omitempty"`
+	Input    *InputTypes    `json:"input,omitempty"`
+	Operator *OperatorTypes `json:"operator,omitempty"`
+	Key      *string        `json:"key,omitempty"`
+	Value    *ValueTypes    `json:"value,omitempty"`
+}
+
+type DataType string
 type InputTypes = string
 type OperatorTypes = string
 type ValueTypes interface {
 }
-type Rule struct {
-	ID       string        `json:"id,omitempty"`
-	Field    string        `json:"field,omitempty"`
-	Type     DataTypes     `json:"type,omitempty"`
-	Input    InputTypes    `json:"input,omitempty"`
-	Operator OperatorTypes `json:"operator,omitempty"`
-	Key      string        `json:"key,omitempty"`
-	Value    ValueTypes    `json:"value,omitempty"`
-}
-
-type WorkloadRule struct {
-	Service         *string         `json:"service,omitempty"`
-	TraceRole       *TraceRole      `json:"trace_role,omitempty"`
-	Protocol        *Protocol       `json:"protocol,omitempty"`
-	ConditionalRule ConditionalRule `json:"conditional_rule,omitempty"`
-}
-
 type Protocol string
 
 const (
@@ -36,70 +55,49 @@ const (
 	HTTP  Protocol = "HTTP"
 )
 
-type TraceRole string
-
 const (
 	server TraceRole = "server"
 	client TraceRole = "client"
 )
 
-type ConditionalRule struct {
-	Condition *Condition `json:"condition,omitempty"`
-	RuleSet   []RuleSet  `json:"rules,omitempty"`
-}
-
-type Condition string
+type TraceRole string
 
 const (
 	AND Condition = "AND"
 	OR  Condition = "OR"
 )
 
-type RuleSet struct {
-	Rule
-	ConditionalRule
+type Condition string
+
+type Filter struct {
+	Type        string       `json:"type"`
+	Condition   Condition    `json:"condition"`
+	Filters     *[]Filter    `json:"filters,omitempty"`
+	WorkloadIds *WorkloadIds `json:"workload_ids,omitempty"`
 }
 
-type FilterRule struct {
-	Version   int                     `json:"version"`
-	Workloads map[string]WorkloadRule `json:"workloads"`
-	FilterId  string                  `json:"filter_id"`
-	Filters   Filters                 `json:"filters"`
-}
+type WorkloadIds []string
 
-type Filters struct {
-	Type        FilterType `json:"type"`
-	Condition   Condition  `json:"condition"`
-	Filters     []Filters  `json:"filters,omitempty"`
-	WorkloadSet []string   `json:"workload_id_set,omitempty"`
-}
-
-type FilterType string
-
-const (
-	Workload FilterType = "workload"
-	Filter   FilterType = "filter"
-)
-
-type Rules []RuleSet
+type Rules []Rule
 
 func (a Rules) Len() int      { return len(a) }
 func (a Rules) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+
 func (a Rules) Less(i, j int) bool {
 	if a[i].Condition == nil && a[j].Condition != nil {
-		sort.Sort(Rules(a[j].RuleSet))
+		sort.Sort(Rules(a[j].Rules))
 		return true
 	} else if a[i].Condition != nil && a[j].Condition == nil {
-		sort.Sort(Rules(a[i].RuleSet))
+		sort.Sort(Rules(a[i].Rules))
 		return false
 	} else if a[i].Condition != nil && a[j].Condition != nil {
 		var ret *bool
-		if *a[i].Condition != *a[j].Condition {
+		if a[i].Condition != a[j].Condition {
 			ret = utils.ToPtr(*a[i].Condition < *a[j].Condition)
 		}
 
-		sort.Sort(Rules(a[i].RuleSet))
-		sort.Sort(Rules(a[j].RuleSet))
+		sort.Sort(Rules(a[i].Rules))
+		sort.Sort(Rules(a[j].Rules))
 
 		str1, err1 := json.Marshal(a[i])
 		str2, err2 := json.Marshal(a[j])
@@ -121,32 +119,32 @@ func (a Rules) Less(i, j int) bool {
 		return *ret
 	} else {
 
-		if a[i].ID != a[j].ID {
-			return a[i].ID < a[j].ID
+		if *a[i].ID != *a[j].ID {
+			return *a[i].ID < *a[j].ID
 		}
 
-		if a[i].Field != a[j].Field {
-			return a[i].Field < a[j].Field
+		if *a[i].Field != *a[j].Field {
+			return *a[i].Field < *a[j].Field
 		}
 
-		if a[i].Type != a[j].Type {
-			return a[i].Type < a[j].Type
+		if *a[i].Datatype != *a[j].Datatype {
+			return *a[i].Datatype < *a[j].Datatype
 		}
 
-		if a[i].Input != a[j].Input {
-			return a[i].Input < a[j].Input
+		if *a[i].Input != *a[j].Input {
+			return *a[i].Input < *a[j].Input
 		}
 
-		if a[i].Operator != a[j].Operator {
-			return a[i].Operator < a[j].Operator
+		if *a[i].Operator != *a[j].Operator {
+			return *a[i].Operator < *a[j].Operator
 		}
 
-		if a[i].Key != a[j].Key {
-			return a[i].Key < a[j].Key
+		if *a[i].Key != *a[j].Key {
+			return *a[i].Key < *a[j].Key
 		}
 
-		x := a[i].Value.(string)
-		y := a[j].Value.(string)
+		x := (*(a[i].Value)).(string)
+		y := (*(a[j].Value)).(string)
 		if x != y {
 			return x < y
 		}
