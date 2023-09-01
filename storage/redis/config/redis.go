8709744c77579add00121f124c3b56e74d85cf64
Mudit Mathur <muditkmathur@gmail.com>
2023-09-01 11:22:14 +0530

    creating a function to get redis connection

--- storage/redis/config/redis.go
+++ storage/redis/config/redis.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"os"
+	"time"
+)
+
 type RedisConfig struct {
 	Host        string         `yaml:"host"`
 	Port        string         `yaml:"port"`
@@ -10,4 +17,15 @@ type RedisConfig struct {
 
 type DB struct {
 	Name string `yaml:"host" env:"DB_HOST" env-description:"Database host"`
+}
+
+func GetRedisConnection(dbName string, redisConfig RedisConfig) *redis.Client {
+	readTimeout := time.Duration(redisConfig.ReadTimeout) * time.Second
+	password := os.Getenv("ZK_REDIS_PASSWORD")
+	return redis.NewClient(&redis.Options{
+		Addr:        fmt.Sprint(redisConfig.Host, ":", redisConfig.Port),
+		Password:    password,
+		DB:          redisConfig.DBs[dbName],
+		ReadTimeout: readTimeout,
+	})
 }
--- storage/redis/versionedStore.go
+++ storage/redis/versionedStore.go
@@ -34,16 +34,9 @@ func GetVersionedStore[T interfaces.ZKComparable](redisConfig *config.RedisConfi
 	if redisConfig == nil {
 		return nil, fmt.Errorf("redis config not found")
 	}
-	readTimeout := time.Duration(redisConfig.ReadTimeout) * time.Second
-	_redisClient := redis.NewClient(&redis.Options{
-		Addr:        fmt.Sprint(redisConfig.Host, ":", redisConfig.Port),
-		Password:    redisConfig.Password,
-		DB:          redisConfig.DBs[dbName],
-		ReadTimeout: readTimeout,
-	})
 
 	versionStore := (&VersionedStore[T]{
-		redisClient:        _redisClient,
+		redisClient:        config.GetRedisConnection(dbName, *redisConfig),
 		versionHashSetName: "zk_value_version",
 		localVersions:      map[string]string{},
 		localKeyValueCache: map[string]*T{},
