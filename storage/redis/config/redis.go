f452657b2210f38be74500f1376c5ea7fe2f9808
avingoyal <avingoyal01+github@gmail.com>
2023-09-06 18:28:16 +0530

    password from redis host config

--- storage/redis/config/redis.go
+++ storage/redis/config/redis.go
@@ -3,7 +3,6 @@ package config
 import (
 	"fmt"
 	"github.com/redis/go-redis/v9"
-	"os"
 	"time"
 )
 
@@ -21,13 +20,13 @@ type DB struct {
 
 func GetRedisConnection(dbName string, redisConfig RedisConfig) *redis.Client {
 	readTimeout := time.Duration(redisConfig.ReadTimeout) * time.Second
-	password := os.Getenv("ZK_REDIS_PASSWORD")
+	//password := os.Getenv("ZK_REDIS_PASSWORD")
 	//host := os.Getenv("ZK_REDIS_HOST")
 	fmt.Print("config.ZK_REDIS_PASSWORD=" + redisConfig.Password)
 	fmt.Print("config.ZK_REDIS_HOST=" + redisConfig.Host)
 	return redis.NewClient(&redis.Options{
 		Addr:        fmt.Sprint(redisConfig.Host, ":", redisConfig.Port),
-		Password:    password,
+		Password:    redisConfig.Password,
 		DB:          redisConfig.DBs[dbName],
 		ReadTimeout: readTimeout,
 	})
