d2422979c8bc269793315ce2d16ee40253ab5f9b
rajeevrajeshuni <rajeev@rajeshuni.com>
2023-11-13 12:08:46 +0530

    Added model for obfuscationRule

---
+++ obfuscation/model/obfuscationRule.go
@@ -0,0 +1,27 @@
+package model
+
+import "time"
+
+type Rule struct {
+	Name       string     `json:"name"`
+	Analyzer   Analyzer   `json:"analyzer"`
+	Anonymizer Anonymizer `json:"anonymizer"`
+	CreatedAt  time.Time  `json:"created_at"`
+	UpdatedAt  time.Time  `json:"updated_at"`
+	CreatedBy  string     `json:"created_by"`
+	Enabled    bool       `json:"enabled"`
+}
+
+type Analyzer struct {
+	Type    string `json:"type"`
+	Pattern string `json:"pattern"`
+}
+
+type Anonymizer struct {
+	Operator string           `json:"operator"`
+	Params   AnonymizerParams `json:"params"`
+}
+
+type AnonymizerParams struct {
+	NewValue string `json:"new_value"`
+}
