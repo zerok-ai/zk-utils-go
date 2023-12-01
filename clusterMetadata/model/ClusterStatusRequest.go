b0972ca5978f66ec82a1bfa136570a7a0354a528
rajeevrajeshuni <rajeev@rajeshuni.com>
2023-12-01 13:18:16 +0530

    Name change and added new models

--- clusterMetadata/model/ClusterStatusRequest.go
+++ clusterMetadata/model/ClusterStatusRequest.go
@@ -4,3 +4,11 @@ type ClusterStatusRequest struct {
 	NumberOfNodes int                      `json:"number_of_nodes"`
 	Services      map[string]ServiceStatus `json:"services"`
 }
+
+type ClusterConnection struct {
+	Connected bool `json:"connected"`
+}
+
+type ClusterConnectionStatus struct {
+	Status map[string]ClusterConnection `json:"status"`
+}
