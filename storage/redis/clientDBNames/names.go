9eb2ac81870ab1e42591d60017d3bc41b575fc7e
vaibhav-paharia <vaibhavpaharia10@gmail.com>
2024-01-03 14:18:29 +0530

    add db number

--- storage/redis/clientDBNames/names.go
+++ storage/redis/clientDBNames/names.go
@@ -1,13 +1,13 @@
 package clientDBNames
 
 const (
-	FilteredTracesDBName       = "filtered_traces"     // 1
-	ScenariosDBName            = "scenarios"           // 2
-	TraceDBName                = "traces"              // 3
-	ExecutorAttrDBName         = "executor_attr"       // 4
-	PodDetailsDBName           = "pod_details"         // 7
-	ErrorDetailDBName          = "error_details"       // 8
-	IntegrationDetailsDBName   = "integration_details" // 9
-	ObfuscationRulesDBName     = "obfuscation_rules"
-	ResourceAndScopeAttrDBName = "resource_and_scope_attr"
+	FilteredTracesDBName       = "filtered_traces"         // 1
+	ScenariosDBName            = "scenarios"               // 2
+	TraceDBName                = "traces"                  // 3
+	ExecutorAttrDBName         = "executor_attr"           // 4
+	ResourceAndScopeAttrDBName = "resource_and_scope_attr" // 5
+	PodDetailsDBName           = "pod_details"             // 7
+	ErrorDetailDBName          = "error_details"           // 8
+	IntegrationDetailsDBName   = "integration_details"     // 9
+	ObfuscationRulesDBName     = "obfuscation_rules"       // 10
 )
