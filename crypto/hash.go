74a68bfe023888072b0ad7d4733e09fe1b5beb55
CHNG7 <nikhilchinta7@gmail.com>
2024-02-26 16:03:54 +0530

    removed unused methods and packages

--- crypto/hash.go
+++ crypto/hash.go
@@ -1,68 +1,11 @@
 package crypto
 
 import (
-	"bytes"
-	"compress/gzip"
 	"crypto/sha1"
-	"errors"
 	"github.com/google/uuid"
-	"github.com/zerok-ai/zk-utils-go/common"
-	"github.com/zerok-ai/zk-utils-go/logs"
-	"io"
 )
-
-var LogTag = "zk_hash"
 
 func CalculateHashNewSHA2(s string) uuid.UUID {
 	hash := sha1.Sum([]byte(s))
 	return uuid.NewSHA1(uuid.Nil, hash[:])
-}
-
-func CompressStringGzip(input string) ([]byte, error) {
-	if common.IsEmpty(input) {
-		logger.Error(LogTag, "empty string")
-		return nil, errors.New("empty input")
-	}
-	var b bytes.Buffer
-
-	gzipWriter := gzip.NewWriter(&b)
-
-	_, err := gzipWriter.Write([]byte(input))
-	if err != nil {
-		logger.Error(LogTag, err)
-		return nil, err
-	}
-
-	err = gzipWriter.Close()
-	if err != nil {
-		logger.Error(LogTag, err)
-		return nil, err
-	}
-
-	return b.Bytes(), nil
-}
-
-func DecompressStringGzip(input []byte) (string, error) {
-	if input == nil || len(input) == 0 {
-		return "", errors.New("empty input")
-	}
-
-	reader := bytes.NewReader(input)
-
-	gzipReader, err := gzip.NewReader(reader)
-	if err != nil {
-		logger.Error(LogTag, err)
-		return "", err
-	}
-	defer gzipReader.Close()
-
-	decompressed, err := io.ReadAll(gzipReader)
-	if err != nil {
-		logger.Error(LogTag, err)
-		return "", err
-	}
-
-	logger.Debug(LogTag, "String decompressed successfully")
-
-	return string(decompressed), nil
 }
--- obfuscation/model/obfuscationResponse.go
+++
@@ -1,4 +0,0 @@
-package model
-
-//TODO: Add a model here for obfuscation response to operator.
-//TODO: Let's use a upsert based response similar to what we are doing for scenario.
